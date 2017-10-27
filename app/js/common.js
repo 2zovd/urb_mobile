814e203602af41c7641f8b8b364bb3847d32b98c
Tuzov <dmy2zov@gmail.com>
2017-10-27 16:55:07 +0300

    sidebar

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 	console.log('Ready!');
+	
+	$(".sidebar").sidebar({
+		side: "left",
+		speed: 500
+	});
+
+	$(".btn-header_nav").click(function() {
+		$(".sidebar").trigger("sidebar:open");
+		$(".sidebar").addClass("opened");
+		$('.sidebar-opened').css('display', 'block');
+	})
+	
+	$(".btn-close-sidebar, .sidebar-opened").click(function() {
+		$(".sidebar").trigger("sidebar:close");
+		$('.sidebar-opened').css('display', 'none');
+	})
+
+	$(".sidebar").on("sidebar:opened", function () {
+		
+	});
+
+	$(".sidebar").on("sidebar:closed", function () {
+	    $('.sidebar-opened').css('display', 'none');
+		$(".sidebar").removeClass('opened')
+	});
+	
+	
 });
+
--- dist/js/common.js
+++ dist/js/common.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+	console.log('Ready!');
 	
-)};
+	$(".sidebar").sidebar({
+		side: "left",
+		speed: 500
+	});
+
+	$(".btn-header_nav").click(function() {
+		$(".sidebar").trigger("sidebar:open");
+	})
+
+
+	$(".my-sidebar").on("sidebar:opened", function () {
+	   // Do something on open
+	});
+
+	$(".my-sidebar").on("sidebar:closed", function () {
+	   // Do something on close
+	});
+	
+	
+});
+
