f3a03883383e4c8ac5a39604599d2eb54a5cf2a1
Tuzov <dmy2zov@gmail.com>
2017-11-06 12:25:09 +0200

    chat build

--- dist/js/common.js
+++ dist/js/common.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
 	});
 	
 	// test letters
-	$('.message-list_row').click(function() {
+	$('.letters-list .message-list_row').click(function() {
 		$('.letters-list').css('display', 'none');
 		$('.letters-message').css('display', 'block');
 		$('.btn-header_nav').css('display', 'none');
@@ -49,8 +49,37 @@ $(document).ready(function() {
 		$('.letters-list').css('display', 'block');
 		$('.letters-message').css('display', 'none');
 		$('.btn-header_nav').css('display', 'block');
-		$('.header-mob_title').html('<span>Letters</span>').removeClass('wrap').removeClass('wrap');
+		$('.header-mob_title').html('<span>Letters</span>').removeClass('wrap');
 	});
+    
+	
+	$('.message-header_nav-btn').click(function() {
+		$(this).toggleClass('active');
+		$('.message-header_nav-drop').toggleClass('active');
+	})
+	
+	// chat test
+	$('.chat-dialog-list .message-list_row').click(function() {
+		$('.chat-dialog-list').css('display', 'none');
+		$('.chat-dialog').css('display', 'block');
+		$('.btn-header_nav').css('display', 'none');
+		$('.btn-header_chat').css('display', 'none');
+		$('.header-mob_title').html('<g><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-left-arrow"></use></svg></g><span>Chat</span>').addClass('wrap');
+		$('.btn-header-mob_chat').addClass('active').html('<button class="btn-chat start-chat">Start chat</button>');
+	});
+	
+	$('.btn-header_chat').click(function() {
+		$('.letters-mob').css('display', 'none');
+		$('.chat-mob').css('display', 'block');
+        $('.header-mob_title').html('<div class="header-mob_title"><span>Chat</span></div>');
+	});
+	
+	
+	//btn chat
+	$('.btn-header-mob_chat').click(function() {
+		$('.btn-chat').toggleClass('start-chat stop-chat');
+	});
+	
 	
 });
 
