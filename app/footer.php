21baa5eb5ab58300e8aac9aebd26a76dd5882824
Tuzov <dmy2zov@gmail.com>
2017-11-08 18:47:05 +0200

    fixes profile photo

--- app/footer.php
+++ app/footer.php
@@ -1,6 +1,4 @@
 
-		</main>
-
 	<!--
 	<footer class="footer-mob">
 		<a href="#" class="footer-mob_logo">
@@ -10,11 +8,95 @@
 	</footer>
 	-->
 
+	<section class="notifications-row">
+		<div class="notifications-slider">
+		
+			<div class="notification-cover notification-invitation">
+				<div class="notification-image">
+					<img src="img/profile-woman_2.jpg" alt="Profile photo">
+				</div>
+				<div class="notification_new-message">
+					<div class="notification-content">
+						<div class="notification-content_top">
+							<strong class="notification_title">Olga, 24</strong>
+							<div class="notification-content_top-right">
+								<button type="button" class="notification-button">Accept</button>
+							</div>
+						</div>
+						<div class="notification-content_bottom">
+							<div class="notification-content_bottom-left">
+								<p class="notification_text">Hello!What do you like in women most of all ?</p>
+							</div>
+							<div class="notification-content_bottom-right">
+								<a class="btn-notification-decline">Decline</a>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="notification-cover">
+					<div class="notification-image">
+						<img src="img/profile-woman_2.jpg" alt="Profile photo">
+					</div>
+				<div class="notification_new-message">
+					<div class="notification-content">
+						<div class="notification-content_top">
+							<strong class="notification_title">New chat message!</strong>
+							<button type="button" class="btn-close-notification">
+								<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#nav-close-ico"></use></svg>
+							</button>
+						</div>
+						<div class="notification-content_bottom">
+							<div class="notification-content_bottom-left">
+								<span class="notification_name">Olga, 24</span>
+								<p class="notification_text">Hello! What do you ...</p>
+							</div>
+							<div class="notification-content_bottom-right">
+								<button type="button" class="notification-button">Read message</button>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+			<div class="notification-cover">
+					<div class="notification-image">
+						<img src="img/profile-woman_2.jpg" alt="Profile photo">
+					</div>
+				<div class="notification_new-message">
+					<div class="notification-content">
+						<div class="notification-content_top">
+							<strong class="notification_title">New chat message!</strong>
+							<button type="button" class="btn-close-notification">
+								<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#nav-close-ico"></use></svg>
+							</button>
+						</div>
+						<div class="notification-content_bottom">
+							<div class="notification-content_bottom-left">
+								<span class="notification_name">Olga, 24</span>
+								<p class="notification_text">Hello! What do you ...</p>
+							</div>
+							<div class="notification-content_bottom-right">
+								<button type="button" class="notification-button">Read message</button>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+			
+		</div>
+	</section>
+
 	</div>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-sidebar/3.3.2/jquery.sidebar.min.js"></script>
-
+	<script src="libs/slick-carousel/slick/slick.min.js"></script>
 	<script src="js/common.js"></script>
 
 </body>
