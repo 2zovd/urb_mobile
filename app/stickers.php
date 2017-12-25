536f355d021934f4eb5282ed1c8dab5df74f6e57
Tuzov <dmy2zov@gmail.com>
2017-12-25 15:25:54 +0200

    gifts and stickers

---
+++ app/stickers.php
@@ -0,0 +1,53 @@
+
+
+<main class="grey-content">
+	<!--stickers-->
+	<div class="content-heading_wrap">
+		<span class="content-heading">Send your emotions to Margarita</span>
+		<p class="content-subheading">Press on sticker to send. You still have 100 stickers to send this month</p>
+	</div>
+
+	<div class="stickers-content">
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/stickers/01.png" alt="Hello beautiful!">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/02.png" alt="Hearts">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/03.png" alt="Good morning!">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/04.png" alt="Kisses">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/05.png" alt="Lets chat">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/06.png" alt="You're beautiful">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/07.png" alt="Send me a letter">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/08.png" alt="Sweet dreams">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/09.png" alt="Flowers">
+		</div>
+
+		<div class="sticker-wrap">
+			<img src="<?php echo base_url(); ?>content/ui/img/img/stickers/10.png" alt="I like your smile">
+		</div>
+
+	</div>
+</main>
