e96f8e3c93ad9317518243ba01ec64e63fa15662
Tuzov <dmy2zov@gmail.com>
2018-03-07 09:26:45 +0200

    contact form

---
+++ app/php/contact_us.php
@@ -0,0 +1,91 @@
+
+<main class="contact-us">
+
+    <p class="contact-title">Feel free to contact us if you have any questions about our services and our website</p>
+
+    <form action="#" class="contact-us_form">
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="Name">
+        </div>
+
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="E-mail adress">
+        </div>
+
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="Skype">
+        </div>
+
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="Link to your Facebook page">
+        </div>
+
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="Phone number">
+        </div>
+
+        <div class="contact-us_form-row">
+            <input type="text" placeholder="Country">
+
+        </div>
+
+        <div class="contact-us_form-row">
+            <textarea placeholder="You can leave your message here"></textarea>
+        </div>
+
+        <div class="contact-us_checkboxes">
+            <p>How would you like us to contact you:</p>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>News and updates</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Phone call</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Viber</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Whatsapp</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Telegramm</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Skype</span>
+            </label>
+
+            <label class="contact-checkbox_row control control--checkbox">
+                <input type="checkbox">
+                <div class="control__indicator"></div>
+                <span>Facebook</span>
+            </label>
+
+        </div>
+
+        <button class="btn-save btn-shadow btn-red">Send</button>
+
+    </form>
+
+    <p></p>
+
+
+
+</main>
