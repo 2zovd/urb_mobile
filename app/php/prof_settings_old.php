6a758ce71c2db31be54cc2523492ee1326cb7ea2
Tuzov <dmy2zov@gmail.com>
2018-03-12 18:40:56 +0200

    fixes

---
+++ app/php/prof_settings_old.php
@@ -0,0 +1,441 @@
+<div class="settings-container settings-content">
+
+<div class="settings-drop_row">
+
+<button type="button" class="settings-drop_header">
+<div class="settings_title-holder_drop">
+<span class="settings-title row-heading">General</span>
+<i class="settings-title_arrow">
+<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-arrow-chevron"></use></svg>
+</i>
+</div>
+<p class="settings-drop_header-text">All fields with <span class="text-star">*</span> are required. Please, set your profile info in fields below.</p>
+</button>
+
+<div class="settings-drop_content">
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">First name <span class="text-star">*</span></label>
+</div>
+<input class="settings_form-field" type="text" placeholder="Name">
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Last name</label>
+</div>
+<input class="settings_form-field" type="text" placeholder="Last name">
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Birthday <span class="text-star">*</span></label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover birth_form-select">
+<select id="#">
+<option value="10">10</option>
+</select>
+</div>
+
+<div class="select-cover birth_form-select">
+<select id="#">
+<option value="09">09</option>
+</select>
+</div>
+
+<div class="select-cover birth_form-select">
+<select id="#">
+<option value="1989">1989</option>
+</select>
+</div>
+
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Country <span class="text-star">*</span></label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Select country">Select country</option>
+<option value="United States">United States</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">City</label>
+</div>
+<div class="settings_select-row">
+<input class="settings_form-field" type="text" placeholder="Your city">
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Language</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="English">English</option>
+<option value="Spanish">Spanish</option>
+</select>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+<div class="settings-drop_row">
+
+<button type="button" class="settings-drop_header">
+<div class="settings_title-holder_drop">
+<span class="settings-title row-heading">Personal</span>
+<i class="settings-title_arrow">
+<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-arrow-chevron"></use></svg>
+</i>
+</div>
+<p class="settings-drop_header-text">Select your personal information</p>
+</button>
+
+<div class="settings-drop_content">
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Height</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="150cm(4' 11'')">150cm(4' 11'')</option>
+<option value="150cm(4' 11'')">155cm(4' 11'')</option>
+<option value="150cm(4' 11'')">160cm(4' 11'')</option>
+</select>
+</div>
+
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Weight</label>
+</div>
+<div class="settings_select-row custom_form-select">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="56kg (123.5 lbs)">56kg (123.5 lbs)</option>
+<option value="56kg (123.5 lbs)">56kg (123.5 lbs)</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Eyes color</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Amber">Amber</option>
+<option value="Blue">Blue</option>
+<option value="Brown">Brown</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Hair color</label>
+</div>
+<div class="settings_select-row">
+<div class="select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Black">Black</option>
+<option value="Brown">Brown</option>
+<option value="Blond">Blond</option>
+</select>
+</div>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Children</label>
+</div>
+<div class="settings_select-row">
+<div class="select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="None">None</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+</select>
+</div>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Religion</label>
+</div>
+<div class="settings_select-row">
+<div class="select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Christian">Christian</option>
+<option value="Pravoslav">Pravoslav</option>
+</select>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<div class="settings-drop_row">
+
+<button type="button" class="settings-drop_header">
+<div class="settings_title-holder_drop">
+<span class="settings-title row-heading">Background</span>
+<i class="settings-title_arrow">
+<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-arrow-chevron"></use></svg>
+</i>
+</div>
+<p class="settings-drop_header-text">Write additional information about you.</p>
+</button>
+
+<div class="settings-drop_content">
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Education</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Second school">Second school</option>
+<option value="College">College</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Drinking</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Negative">No</option>
+<option value="Neutral">Socially</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Smoking</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="No">No</option>
+<option value="Socially">Socially</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">About me</label>
+</div>
+<textarea class="settings_form-textarea" placeholder="Write something about your self."></textarea>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">My hobbies</label>
+</div>
+<textarea class="settings_form-textarea" placeholder="Write about your hobbies."></textarea>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">My profession</label>
+</div>
+<textarea class="settings_form-textarea" placeholder="Write about your work."></textarea>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_row">
+
+<button type="button" class="settings-drop_header">
+<div class="settings_title-holder_drop">
+<span class="settings-title row-heading">Partner</span>
+<i class="settings-title_arrow">
+<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-arrow-chevron"></use></svg>
+</i>
+</div>
+<p class="settings-drop_header-text">In the fields below, select qualities you see your partner.</p>
+</button>
+
+<div class="settings-drop_content">
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Her Age <span class="text-star">*</span></label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover double_form-select">
+<select id="#">
+<option value="From">From</option>
+<option value="18">18</option>
+<option value="19">19</option>
+</select>
+</div>
+<div class="select-cover double_form-select">
+<select id="#">
+<option value="To">To</option>
+<option value="50">50</option>
+<option value="51">51</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Children</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="None">No</option>
+<option value="1">1</option>
+<option value="2">2</option>
+<option value="3">3</option>
+<option value="4">4</option>
+<option value="5">5</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Drinking</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Negative">Negative</option>
+<option value="Neutral">Neutral</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">Smoking</label>
+</div>
+<div class="settings_select-row">
+<div class="select-cover custom_form-select">
+<select id="#">
+<option value="Negative">Negative</option>
+<option value="Neutral">Neutral</option>
+</select>
+</div>
+</div>
+</div>
+
+<div class="settings-drop_form-row">
+<div class="settings_title-holder">
+<label class="settings-title">About my partner</label>
+</div>
+<textarea class="settings_form-textarea" placeholder="Write something about the way you see your partner"></textarea>
+</div>
+
+</div>
+</div>
+
+<div class="settings-drop_row">
+
+<button type="button" class="settings-drop_header">
+<div class="settings_title-holder_drop">
+<span class="settings-title row-heading">My Account</span>
+<i class="settings-title_arrow">
+<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ico-arrow-chevron"></use></svg>
+</i>
+</div>
+<p class="settings-drop_header-text">Here you can change your account settings.</p>
+</button>
+
+<div class="settings-drop_content">
+
+<div class="settings-drop_form-row form-border">
+<div class="settings_title-holder">
+<label class="settings-title">Password</label>
+</div>
+<input class="settings_form-field" type="password" placeholder="New password">
+<input class="settings_form-field" type="password" placeholder="Confirm password">
+<button type="button" class="btn-change">Change</button>
+</div>
+
+<div class="settings-drop_form-row form-border">
+<div class="settings_title-holder">
+<label class="settings-title">E-mail</label>
+</div>
+<input class="settings_form-field" type="text" placeholder="mymail@mail.com">
+<button type="button" class="btn-change">Change</button>
+</div>
+
+<div class="settings-drop_form-row drop-checkbox">
+<div class="settings_title-holder">
+<label class="settings-title">Notifications</label>
+</div>
+<label class="setting-checkbox_row control control--checkbox">
+<input type="checkbox">
+<div class="control__indicator"></div>
+<span>News and updates</span>
+</label>
+<label class="setting-checkbox_row control control--checkbox">
+<input type="checkbox" checked>
+<div class="control__indicator"></div>
+<span>Messages and invitations</span>
+</label>
+<label class="setting-checkbox_row control control--checkbox">
+<input type="checkbox" >
+<div class="control__indicator"></div>
+<span>Promo</span>
+</label>
+</div>
+
+</div>
+
+
+</div>
+
+</div>
